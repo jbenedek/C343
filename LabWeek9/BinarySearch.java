c4ba3e257145e44924bebdcd536e5b05703619e9
Jeremy Benedek <jerbened@indiana.edu>
2016-03-09 20:07:55 -0500

    Lab finished and showed to AI in Wednesday Lab Session

--- LabWeek9/BinarySearch.java
+++ LabWeek9/BinarySearch.java
@@ -5,15 +5,18 @@ public class BinarySearch <E extends Comparable<?super E>>{
 
   public static void main(String[] args){
 	Integer[] a = new Integer[] {2, 3, 4, 5, 6, 7, 8, 9};
-	boolean[] b= {false, false, false, true};
-	int[] d = {1,2,3,4,5};
-	boolean[] c = {false, false};
+	String[] s = {"a", "b", "c", "d"};
+	BinarySearch bs = new BinarySearch();	
 	
-	System.out.println("Searching for 4 in :" + Arrays.toString(a) + searchAndReturnIndex(a, 4) );
+	System.out.println("Searching for 4 in :" + Arrays.toString(a) + " " + bs.searchAndReturnIndex(a, 4) );
+	System.out.println("Searching for 12 in :" + Arrays.toString(a) + " " + bs.searchAndReturnIndex(a, 12) );
+	System.out.println("Searching for b in :" + Arrays.toString(s) + " " + bs.searchAndReturnIndex(s, "b") );
+	System.out.println("Searching for w in :" + Arrays.toString(s) + " " + bs.searchAndReturnIndex(s, "w") );
+
 
   }
 
-  public int searchAndReturnIndex( E[] a, E k){
+  public int searchAndReturnIndex(E[] a, E k){
 	int l = -1;
 	int r = a.length;
 	while(l+1 != r){
